20c310b47fbb83ac30153e5b5ebd363a626a6c01
zhonghua.wu <zhonghua.wu@envision-digital.com>
2019-06-29 14:59:05 +0800

    add .

---
+++ src/com/huaa/chapter4/Mutex.java
@@ -0,0 +1,33 @@
+package com.huaa.chapter4;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Desc:
+ *
+ * @author zhonghua.wu
+ * @date 2019/6/29 11:11
+ */
+public class Mutex {
+
+    private final static Object MUTEX = new Object();
+
+    public void accessResource() {
+        synchronized (MUTEX) {
+            try {
+                TimeUnit.MINUTES.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        final Mutex mutex = new Mutex();
+        for(int i=0; i<5; i++) {
+            new Thread(mutex::accessResource).start();
+        }
+    }
+
+
+}
