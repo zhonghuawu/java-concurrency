144e7f1a62faa23bef729dbb4fd069aafa6c9eb2
zhonghuawu <wu_zhonghua@outlook.com>
2019-07-07 18:55:36 +0800

    add chapter10, test bootstrap classloader

---
+++ src/com/huaa/chapter10/BootstrapClassLoader.java
@@ -0,0 +1,26 @@
+package com.huaa.chapter10;
+
+/**
+ * Desc:
+ *
+ * @author huaa
+ * @date 7/7/19 5:02 PM
+ */
+public class BootstrapClassLoader {
+
+    public static void main(String[] args) {
+        System.out.println("Bootstrap: " + String.class.getClassLoader());
+        System.getProperties().list(System.out);
+
+        try {
+            Class<?> cls = Class.forName("com.huaa.chapter10.BootstrapClassLoader");
+            System.out.println(cls.getClassLoader());
+            System.out.println(cls.getClassLoader().getParent());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
